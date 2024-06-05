2c52490e1e82d3521f5632b1843d8fb76e0d6324
caggieb <cefblaszyk@gmail.com>
2024-06-05 23:12:01 +0200

    commit

--- physics.py
+++ physics.py
@@ -1,6 +1,27 @@
 import numpy as np
 import time
 
+f = 10 #shot power
+f_multiplier = 0.02 
+
+velo = [0,0] #starting velocity
+posi = [0,0] #starting position
+direct = np.pi/2   #starting direction in rads
+
+def force(speed):
+    force = -f_multiplier * speed
+    return force
+
+def bounce(state, direction):
+    if state == "horizontal":
+        direction = np.pi - direction
+    if state == "vertical":
+        direction = (direction - 2 * np.pi)* -1
+    else:
+        direction = direction
+    return direction
+
+
 def acvel(force, direction, vel):
     accx = force * np.cos(direction)
     accy = force * np.sin(direction)
@@ -28,15 +49,15 @@ def position(vel, pos):
     y += vel[1]
     return x, y 
 
-f = 10
-velo = [0,0]
-posi = [0,0]
 
-while True:
+#purely testing purpose
+while abs(acvel(f,0,velo)[1]) > 0.4:
     
-    velo = acvel(f,0,velo)[0]
+    velo = acvel(f,direct,velo)[0]
     posi = position(velo, posi)
+    direct = bounce(0,direct)
     print(posi)
-    print(velo)
-    f = 0
+    print(acvel(f,direct,velo))
+
+    f = force(acvel(f,direct,velo)[1])
     time.sleep(0.1)
